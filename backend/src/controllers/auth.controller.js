4d046d6820fcb808f80ad03b26d0ffa073af24f0
gsantosh260402 <gsantosh260402@gmail.com>
2026-02-01 17:17:16 +0530

    login & logout controllers added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -12,7 +12,7 @@ export const signup = async (req , res)=>{
         if(!fullName || !email || !password){
             return res.status(400).json({message : "All fields are Required"});
         }
-
+    
         if(password.length < 6){
             return res.status(400).json({message : "Password must be atleast 6 characters"});
         }
@@ -62,4 +62,41 @@ export const signup = async (req , res)=>{
          console.log("Error in signup controller . " , error);
          res.status(500).json({message : "Internal Server Error"})
     }
+}
+
+export const login = async(req , res)=>{
+
+    const {email , password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+        if(!user)
+           return res.status(400).json({message : "Invalid Credentials"});
+
+        const isPasswordCorrect = await bcrypt.compare(password , user.password);
+
+        // bcrypt db me stored passwordse salt nikal ta hai phir , new password ko usssi salt ke saath hash karta hai 
+        // phir iss hashed password ko db me stored paasword se compare karta hai
+        if(!isPasswordCorrect)
+             return res.status(400).json({message : "Invalid Credentials"});
+
+        generateToken(user._id , res);
+
+        res.status(201).json({
+            _id : user._id,
+            fullName : user.fullName,
+            email:user.email,
+            profilePic : user.profilePic,
+        })
+
+
+    }catch(error){
+        console.error("Error in login Controller:" , error)
+        res.status(500).json({message:"Internal server error"});
+    }
+}
+
+export const logout = (_ , res)=>{
+    res.cookie("jwt" , "" , {maxAge:0});
+    res.status(200).json({message : "Logged Out Successfully"})
 }
