20e040725590fc342993c6c3621a6802c42d53ce
gsantosh260402 <gsantosh260402@gmail.com>
2026-02-01 23:40:37 +0530

    update-profile controller added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { sendWelcomeEmail } from '../emails/emailHandlers.js';
 import { generateToken } from '../lib/utils.js';
 import User from '../models/User.js'
 import bcrypt from 'bcryptjs'
+import cloudinary from '../lib/cloudinary.js'
 import "dotenv/config";
 
 export const signup = async (req , res)=>{
@@ -103,4 +104,23 @@ export const login = async(req , res)=>{
 export const logout = (_ , res)=>{
     res.cookie("jwt" , "" , {maxAge:0});
     res.status(200).json({message : "Logged Out Successfully"})
+}
+
+export const updateProfile = async(req , res) =>{
+    try{
+
+       const {profilePic} = req.body;
+       if(!profilePic) return res.status(400).json({message : "Profile pic is required"});
+       
+       const userId = req.user._id;
+       const uploadResponse = await cloudinary.uploader(profilePic);
+
+       const updatedUser = await User.findByIdAndUpdate(userId , {profilePic : uploadResponse.secure_url} , {new : true});
+
+       return res.status(200).json(updatedUser);
+
+    }catch(error){
+       console.log("Error in update profile :" , error);
+       res.status(500).json({message : "Internal server error"});
+    }
 }
