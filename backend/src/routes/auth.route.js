75fa5ea66e286ad23bbe3338ccb09fe93d8e6f36
gsantosh260402 <gsantosh260402@gmail.com>
2026-04-16 00:02:43 +0530

    Message Routes Added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,7 +4,7 @@ import { protectRoute } from '../middlewares/auth.middleware.js';
 import {arcjetProtection} from "../middlewares/arcjet.middleware.js"
 const router = express.Router();
 
-router.use(arcjetProtection);
+// router.use(arcjetProtection);
 router.get("/test"  , (req , res)=>{
     res.status(200).json({message : " Test route "})
 });
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,7 +1,12 @@
 import express from 'express';
 const router = express.Router();
+import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage } from '../controllers/message.controller.js';
+import { protectRoute } from '../middlewares/auth.middleware.js';
 
-router.get("/send" , (req , res)=>{
-    res.send("Send Message Endpoint");
-})
+router.use(protectRoute);
+router.get("/contacts" , getAllContacts);
+router.get("/chats" , getChatPartners);
+router.get("/:id" , getMessagesByUserId);
+router.post("/send/:id" , sendMessage );
+
 export default router;
